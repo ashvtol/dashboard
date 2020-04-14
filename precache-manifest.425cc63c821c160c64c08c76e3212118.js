7c0b3dcc7e875e57d50075ed2e40bbf413e2fdf4
Ashish Kumar <ash.vtol@gmail.com>
2020-04-14 17:16:49 -0400

    Updates

---
+++ precache-manifest.425cc63c821c160c64c08c76e3212118.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9482390f5f69f223aeca62941335b53b",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "4111c6cfb403c9030f8a",
+    "url": "/dashboard/static/css/main.45f8ac7d.chunk.css"
+  },
+  {
+    "revision": "9337c15ffd49974a9ff4",
+    "url": "/dashboard/static/js/2.860f1856.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/dashboard/static/js/2.860f1856.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4111c6cfb403c9030f8a",
+    "url": "/dashboard/static/js/main.1e498230.chunk.js"
+  },
+  {
+    "revision": "8f629599e45097e5ce3c",
+    "url": "/dashboard/static/js/runtime-main.3bb16907.js"
+  }
+]);
